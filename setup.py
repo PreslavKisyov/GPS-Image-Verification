0daf5dfb3c4ab9ffd0e5b66000e7da23a94720b9
Preslav Kisyov <preslavkisyov98@gmail.com>
2020-04-13 14:18:41 +0100

    Add setup exe

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='GPS-Image-Verification',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='Public',
+    author='Preslav Kisyov',
+    author_email='preslav.kisyov@kcl.ac.uk',
+    description='Imaged-Based GPS Verification'
+)
